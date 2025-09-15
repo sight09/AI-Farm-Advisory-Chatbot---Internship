a0c8a747e3f3900dd634e5f66dbd4272d4973635
Mr-robot <hackingtest017@gmail.com>
2025-09-15 18:56:20 +0300

    pydantic settings

---
+++ backend/common/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict # type: ignore
+
+class Settings(BaseSettings):
+    openai_api_key: str = ""
+    gen_model: str = "gpt-4o-mini"
+    embedding_model: str = "text-embedding-3-small"
+    embed_dim: int = 1536
+    database_url: str = ""
+    k_retrieval: int = 5
+    
+    allowed_file_types: list = [".pdf"]
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+settings = Settings()
