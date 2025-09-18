9dbc5d7dc1799bed2171c873907bbf96cdc9fa94
Mr-robot <hackingtest017@gmail.com>
2025-09-18 10:34:37 +0300

    integrate weather

--- backend/app/routers/chat_bot.py
+++ backend/app/routers/chat_bot.py
@@ -50,14 +50,17 @@ async def ask_question(request: AskRequest, db: Session = Depends(get_db)):
     if not context.strip():
         return AskResponse(answer="I'm sorry, I don't have enough information to answer that question.", sources=[])
 
-    # if request.location:
-    #     weather_data = await get_weather(request.location)
-    #     context += f"\n\nWeather information for {request.location}:\n{weather_data}"
+
+    weather_data = None
+    if request.location:
+        weather_data = await get_weather(request.location)
+        context += f"\n\nWeather information for {request.location}:\n{weather_data}"
 
     # Build prompt for answering
     system = (
         "You are an agricultural assistant. Use ONLY the provided context to answer the question in clear simple English. "
         "If answer not in context, be honest and say you don't know."
+        "If the user ask for the weather, provide a concise weather report using the provided data, without any additional commentary."
         f"Answer must be in {lang_map[request.lang]} language only don't include any other language."
     )
     
@@ -67,13 +70,16 @@ async def ask_question(request: AskRequest, db: Session = Depends(get_db)):
 
     Context:
     {context}
+    Weather Data:
+    {weather_data if weather_data else 'No weather data provided.'}
     """
 
     answer = await chat_completion(system, [{"role":"user","content":prompt}], max_tokens=512)
     
-    # print("Question:", request.question)
-    # print("Context:", context)
-    # print("Answer:", answer)
+    print("Question:", request.question)
+    print("Context:", context)
+    print("Weather info:", weather_data)
+    print("Answer:", answer)
 
     sources = ["source1", "source2"]
     return AskResponse(answer=answer, sources=sources)
