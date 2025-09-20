9b8644ac741183650cd83c11a52b28f8d432baf3
Mr-robot <hackingtest017@gmail.com>
2025-09-20 19:42:48 +0300

    trying to get location from the bot

--- backend/services/bot/handlers/handle_message.py
+++ backend/services/bot/handlers/handle_message.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
 import httpx
 
@@ -6,6 +6,11 @@ import httpx
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Send a message when the command /start is issued."""
     print("Bot started...")
+    
+    # button = KeyboardButton("Send my location", request_location=True)
+    # keyboard = [[button]]
+    # reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+    
     user = update.effective_user
     return await update.message.reply_html(
         rf"Hi {user.mention_html()}! Welcome to the AI Farm Advisory Chatbot. How can I assist you today?"
@@ -14,6 +19,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Echo the user message."""
     user_message = update.message.text
+    
+    latitude = update.message.location.latitude if update.message.location else None
+    longitude = update.message.location.longitude if update.message.location else None
+    print(f"Received location: lat={latitude}, lon={longitude}")
     
     # interact with /ask endpoint
     async with httpx.AsyncClient(timeout=None) as client:
