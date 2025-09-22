55ba39380a144ceaf74f05a9430a960f456c2223
Mr-robot <hackingtest017@gmail.com>
2025-09-22 20:44:05 +0300

    model update

--- backend/common/models/user.py
+++ backend/common/models/user.py
@@ -5,5 +5,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
+    telegram_id = Column(String, unique=True, index=True)
+    language = Column(String, default="en")
+    location = Column(String)
+    email = Column(String, unique=True, index=True, nullable=True)
     hashed_password = Column(String)
