811de793d36757fae47851a99edef35e63dff8df
Mr-robot <hackingtest017@gmail.com>
2025-09-22 20:43:56 +0300

    integrate change handler

--- backend/services/bot/handlers/bot.py
+++ backend/services/bot/handlers/bot.py
@@ -1,13 +1,20 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from common.config import settings
 from services.bot.handlers.handle_message import start, handle_message
+from services.bot.handlers.language_changer import language_change_handler
+from services.bot.handlers.location_changer import location_change_handler
 
 
 def init_bot():
     app = ApplicationBuilder().token(settings.telegram_bot_token).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(language_change_handler)
+    app.add_handler(location_change_handler)
+    
+    
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
 
     print("Bot is running...")
     app.run_polling()
